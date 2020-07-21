4ca21f3978aaec40b6c73123f51ca85c8a79af2c
Jamie Schrifrin <jamieschifrin@hotmail.com>
2020-07-21 00:46:43 -0400

    first

---
+++ app.js
@@ -0,0 +1,68 @@
+var posts = [{
+    title: 'Welcome to Space...Assholes',
+    picture: "./assets/logo.jpg",
+    post: "at eaque aspernatur nostrum impedit vero pariatur dignissimos ? Commodi ut architecto voluptates! Unde voluptatibus magnam expedita accusamus possimus necessitatibus debitis, neque quibusdam provident ? Veritatis molestias voluptate magnam, aliquid voluptatem eius possimus in !Rem expedita ipsa cum enim dolorem iste quo est quas corporis aut autem, consequatur illum, deserunt velit, ipsum vel dignissimos."
+},
+    {
+        title: 'Launch Pad 7',
+        picture: "./assets/1.jpg",
+            post: "dolor sit, amet consectetur adipisicing elit.Commodi corporis minus ipsum facilis.Minima laboriosam eos hic earum.Blanditiis, laboriosam libero ? Voluptates fugit unde rem dolor quia, facilis vel quibusdam, exercitationem nostrum recusandae quo alias maxime qui ratione accusantium ab iusto error laboriosam ? Provident deserunt explicabo consectetur, nulla autem at accusantium exercitationem vitae quod, mollitia inventore natus possimus, cum nemo maxime quisquam nostrum animi incidunt! Tenetur est voluptatem at.Nobis at blanditiis nostrum quasi voluptatum porro explicabo amet eum laudantium tempore inventore nihil quas quos aut id vitae, maiores vel! Dolor esse cumque "
+    },
+    {
+        title: '3..2..1..Liftoff!',
+        picture: "./assets/2.jpg",
+        post: 'error hic recusandae id praesentium maiores nihil delectus fugit dolore voluptatibus, laudantium mollitia nobis rem neque ipsum modi reiciendis.Enim ullam blanditiis, sapiente et nesciunt numquam consequatur error deleniti ? Explicabo itaque quisquam nulla maiores ex aperiam eveniet vel dolor voluptatem animi obcaecati, veniam similique dolorum quas officiis sint cum.Suscipit omnis in aliquam consequuntur officiis.Voluptatibus commodi consectetur nam error corrupti consequuntur quia adipisci delectus dolorum doloremque facere non nesciunt amet saepe tenetur, ratione sint deserunt esse alias optio iste qui autem exercitationem molestiae ? Eos, quis.Soluta quam tempore obcaecati blanditiis velit error tenetur ex repudiandae, dicta aperiam fugiat, porro iste quaerat similique modi assumenda consequatur rem provident '
+    },
+    {
+        title: 'Meet Bob & Doug',
+        picture: "./assets/3.jpg",
+        post: 'laborum reprehenderit vitae deleniti officia.Earum quis animi, assumenda inventore voluptatibus provident placeat hic amet voluptas maiores voluptate ut fugiat error.Porro assumenda vero laudantium enim autem veniam eum id asperiores quaerat libero.Necessitatibus, adipisci sapiente rem animi voluptatibus odio, cum placeat, impedit esse dolor cumque ? Error doloremque eaque itaque ab quia libero non aperiam unde accusantium, dolor laudantium minus, quos atque illum modi eveniet molestias impedit natus minima totam explicabo doloribus alias ? Praesentium, exercitationem reprehenderit sint necessitatibus ea dolorem voluptatibus non officia placeat fugit delectus animi voluptas sed sunt odio voluptatem asperiores, nesciunt minima, obcaecati voluptatum ? Pariatur, error porro! Tempore fuga, aut tempora itaque fugiat labore fugit.Ut perferendis magni facere molestiae velit possimus labore.Nulla nihil, sed ullam excepturi illo fugit blanditiis officiis'
+    },
+    {
+        title: ' Escape Velocity',
+        picture: "./assets/4.jpg",
+        post: ' Atque voluptatem totam eligendi inventore dignissimos tempore doloremque culpa blanditiis maiores perspiciatis qui voluptates eveniet iusto, consequuntur amet a corporis, adipisci ipsam molestiae obcaecati magni, animi exercitationem alias ? Fugit velit esse, harum doloremque aut quis voluptatem obcaecati accusantium minus, ipsum odio ? Blanditiis quasi ipsam facere natus.Rem tenetur, minus ratione vel vitae repellendus, sapiente et sint ipsum adipisci, accusamus repudiandae ? Sunt eum modi nihil vitae laborum, iure unde repellendus commodi debitis ipsa perferendis esse natus molestiae dolores dicta ea itaque nisi.Cum ad sequi, quaerat expedita illo earum est saepe in mollitia, non accusamus, quae cumque rerum facilis repellat dolor fuga corrupti voluptates temporibus.Nisi, labore ipsa.'
+    },
+    {
+        title: 'Dragon Docking',
+        picture: "./assets/5.jpg",
+        post: 'Omnis sint unde iusto laboriosam distinctio numquam, architecto perferendis autem placeat earum, animi quam, sequi vero nisi suscipit dolorum nobis nihil consequatur fugit temporibus quas officia ? Nulla nihil reprehenderit perspiciatis ad eaque.Obcaecati perspiciatis impedit provident consequuntur, quidem aliquam! Temporibus accusantium ab, veniam aut eius iure nisi error autem vero quo, quas quibusdam quae vel aspernatur excepturi at ? '
+    },
+    {
+        title: '57 landings and counting',
+        picture: "./assets/6.jpg",
+        post: 'harum ipsa dolorum ipsum, vitae dignissimos alias possimus vero, fugit cumque hic, sed enim consectetur.Nihil accusantium odit provident distinctio quibusdam alias, cum tenetur hic voluptates repellendus neque at beatae obcaecati! Atque dolorum consectetur magnam quo officia laudantium alias aliquam, id dignissimos, praesentium dolorem dolor deleniti laborum eos repudiandae a, facere velit suscipit provident cupiditate?'
+    },
+    {
+        title: 'The Falcon 9',
+        picture: "./assets/7.jpg",
+        post: 'sequi nisi, animi dolores amet libero expedita quam eligendi distinctio iusto nam quos qui facere quaerat voluptate ? Officiis, optio ? Vel, vitae ipsam blanditiis quibusdam deleniti dicta commodi incidunt maxime, natus accusantium adipisci minus aperiam tempora sit omnis quos ab doloribus eum reprehenderit! Ad accusamus facilis illo totam nostrum distinctio rem assumenda voluptatem ex libero, dignissimos quidem inventore suscipit eaque aliquam itaque a non modi blanditiis ratione voluptatum impedit ? Eos quia unde consectetur amet laborum dolorem nesciunt ut ab magni dolores repellendus eum temporibus, suscipit distinctio ipsum molestias alias non repellat pariatur libero perferendis est ?'
+    },
+    {
+        title: 'The Crew Dragon',
+        picture: "./assets/8.jpg",
+        post: 'doloremque dignissimos voluptatibus veritatis quidem eum molestias cum officia repellat nulla necessitatibus porro, in dolorum ? Aspernatur possimus eaque laborum, optio dolorum in sapiente veritatis cumque eos accusamus iure nesciunt repellendus, explicabo magnam officiis eveniet culpa beatae reprehenderit quos harum obcaecati necessitatibus labore nulla ? Ut maxime eligendi error.Soluta dicta corporis quia fugiat perferendis repellat ducimus corrupti officiis fugit temporibus reprehenderit nulla suscipit a et quidem at rem molestias, tempore magni provident.Nulla laudantium fugiat corrupti dolor officiis voluptate molestias laboriosam omnis.'
+        
+    },
+    {
+        title: 'Leaving the Planet',
+        picture: "./assets/9.jpg",
+        post: ' debitis odio porro provident sint nesciunt corrupti cum reiciendis vero officiis, quia voluptatum est impedit nemo! Nulla delectus laborum ab aut maiores minus, praesentium facilis exercitationem ipsum corporis nam a quas harum amet perferendis deserunt hic.Omnis, est rerum.Perspiciatis corporis id totam, sed asperiores placeat quibusdam ratione esse eos, minima quia tempore ullam nostrum, cumque iusto accusantium magnam perferendis aliquam sapiente culpa.Hic ipsum ipsam aut similique dolor commodi dolore esse eos eius repudiandae earum officiis neque, exercitationem, temporibus, et voluptatum est illum inventore modi! Quos aliquid hic, accusamus consequuntur velit vero ad autem nihil enim cumque qui ut fugit quo voluptates, ducimus, ipsam dolorum doloribus optio! Cum quaerat esse voluptatibus qui consectetur, illo accusamus corporis dicta animi adipisci necessitatibus, hic aliquam, debitis accusantium assumenda magnam illum ad.Eaque perferendis quaerat unde hic assumenda eos quo, et quisquam.Modi maxime doloribus ducimus laborum! Deserunt reprehenderit exercitationem facilis blanditiis tempora voluptas, similique maiores quas.'
+    },
+];
+var postHTML =''
+for (var i = 0; i <posts.length; i++) {
+    var heading = "<div class = 'post'><h2>"+posts[i].title+"</h2>";
+    var mainImage = "<img src = '"+posts[i].picture+"'>";
+    
+    var body = "<p>"+posts[i].post+"</p></div>";
+    var concatThis = heading + mainImage + body;
+    postHTML = postHTML + concatThis;
+    console.log(mainImage)
+};
+
+console.log(postHTML)
+
+document.getElementById('blogPosts').innerHTML = postHTML;
+
+
